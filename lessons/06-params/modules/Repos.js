4c8ff26796eb96719a280b5408549a1e5293fc80
tsu1ro <tsu1ro@gmail.com>
2017-09-09 14:36:50 +0900

    done lesson6

--- lessons/06-params/modules/Repos.js
+++ lessons/06-params/modules/Repos.js
@@ -1,7 +1,17 @@
 import React from 'react'
+import { Link } from 'react-router'
+import NavLink from './NavLink'
 
 export default React.createClass({
-  render() {
-    return <div>Repos</div>
-  }
+	render(){
+		return (
+			<div>
+				<h2>Repos</h2>
+				<ul>
+					<li><NavLink to="/repos/reactjs/react-router">React Router</NavLink></li>
+					<li><NavLink to="/repos/facebook/react">React</NavLink></li>
+				</ul>
+			</div>
+		)
+	}
 })
